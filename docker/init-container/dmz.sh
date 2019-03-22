c320a8606da5110d1bd70d74d9d6647f6f6692ed
nyg <nyg@users.noreply.github.com>
2019-03-23 00:19:50 +0100

    Added some scripts to automate container configuration.

---
+++ docker/init-container/dmz.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+# remove default route and create new one to go through firewall only
+ip route del default
+ip route add default via 192.168.200.2
+
+# start bash to keep container running
+/bin/bash
---
+++ docker/scripts/run-containers.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# run firewall
+docker run -di --cap-add=NET_ADMIN --cap-add=NET_RAW --name firewall -h Firewall srx/firewall-lab /init-container/firewall.sh
+
+# connect firewall to dmz and lan networks
+docker network connect lan firewall
+docker network connect dmz firewall
+
+# run dmz and lan containers
+docker run -di --net dmz --cap-add=NET_ADMIN --cap-add=NET_RAW --name dmz -h ServerInDMZ srx/firewall-lab /init-container/dmz.sh
+docker run -di --net lan --cap-add=NET_ADMIN --cap-add=NET_RAW --name lan -h ClientInLAN srx/firewall-lab /init-container/lan.sh
